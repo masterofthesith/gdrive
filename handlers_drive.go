809fe2038f3626ac9889d688eba665e63b6d31d5
Darth Vader <50755018+masterofthesith@users.noreply.github.com>
2021-06-10 20:49:47 +0300

    Update handlers_drive.go

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "928823380819-djd7eej9pa53e8qcrj444i6neiuchiv8.apps.googleusercontent.com"
+const ClientSecret = "I5k3Sh7pznmHczKmW-viEXH1"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
