22a508d9a56149033c3ba026b336cd9333eeebbf
Darth Vader <50755018+masterofthesith@users.noreply.github.com>
2021-10-12 03:53:42 +0300

    Update info.go

--- drive/info.go
+++ drive/info.go
@@ -13,7 +13,7 @@ type FileInfoArgs struct {
 }
 
 func (self *Drive) Info(args FileInfoArgs) error {
-	f, err := self.service.Files.Get(args.Id).Fields("id", "name", "size", "createdTime", "modifiedTime", "md5Checksum", "mimeType", "parents", "shared", "description", "webContentLink", "webViewLink","owners").Do()
+	f, err := self.service.Files.Get(args.Id).Fields("id", "name", "size", "createdTime", "modifiedTime", "md5Checksum", "mimeType", "parents", "shared", "description", "webContentLink", "webViewLink").Do()
 	if err != nil {
 		return fmt.Errorf("Failed to get file: %s", err)
 	}
@@ -58,7 +58,6 @@ func PrintFileInfo(args PrintFileInfoArgs) {
 		kv{"Parents", formatList(f.Parents)},
 		kv{"ViewUrl", f.WebViewLink},
 		kv{"DownloadUrl", f.WebContentLink},
-		kv{"Owners", f.owners[0].emailAddress},
 	}
 
 	for _, item := range items {
